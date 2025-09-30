13cd20dfe9986d2068a220a089bcf4016dbade65
Anatoliy Kobzev <Kobzev-a-e@yandex.ru>
2025-09-30 21:30:26 +0300

    not finished yet :(

---
+++ tb_top.sv
@@ -0,0 +1,148 @@
+`timescale 1ns/1ps
+
+module tb_stream_arbiter_w_qos();
+
+parameter T_DATA_WIDTH = 4;
+parameter T_QOS__WIDTH = 2;
+parameter STREAM_COUNT = 2;
+localparam T_ID___WIDTH = $clog2(STREAM_COUNT);
+logic clk;
+logic rst_n;
+
+// Input streams
+logic [T_DATA_WIDTH-1:0] s_data_in [STREAM_COUNT-1:0];
+logic [T_QOS__WIDTH-1:0] s_qos_in [STREAM_COUNT-1:0];
+logic [STREAM_COUNT-1:0] s_last_in;
+logic [STREAM_COUNT-1:0] s_valid_in;
+logic [STREAM_COUNT-1:0] s_ready_out;
+
+// Output stream
+logic [T_DATA_WIDTH-1:0] m_data_out;
+logic [T_QOS__WIDTH-1:0] m_qos_out;
+logic [T_ID___WIDTH-1:0] m_id_out;
+logic m_last_out;
+logic m_valid_out;
+logic m_ready_in;
+
+// Clock generation
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+// DUT
+stream_arbiter_w_qos #(
+    .T_DATA_WIDTH(T_DATA_WIDTH),
+    .T_QOS__WIDTH(T_QOS__WIDTH),
+    .STREAM_COUNT(STREAM_COUNT),
+    .T_ID___WIDTH(T_ID___WIDTH)
+) dut (
+    .clk(clk),
+    .rst_n(rst_n),
+    
+    .s_data_in(s_data_in),
+    .s_qos_in(s_qos_in),
+    .s_last_in(s_last_in),
+    .s_valid_in(s_valid_in),
+    .s_ready_out(s_ready_out),
+    
+    .m_data_out(m_data_out),
+    .m_qos_out(m_qos_out),
+    .m_id_out(m_id_out),
+    .m_last_out(m_last_out),
+    .m_valid_out(m_valid_out),
+    .m_ready_in(m_ready_in)
+);
+
+initial begin
+    // Initialize
+    rst_n = 0;
+    m_ready_in = 1;
+    s_valid_in = 0;
+    s_last_in = 0;
+    foreach(s_data_in[i]) begin
+        s_data_in[i] = 0;
+        s_qos_in[i] = 0;
+    end
+    
+    // Reset
+    #20;
+    rst_n = 1;
+    #10;
+    
+    $display("----------------- Test 1: Basic QoS Priority ------------------------");
+    
+
+    s_valid_in[0] = 0;
+    s_data_in[0] = 4'hA;
+    s_qos_in[0] = 2'b01; 
+    s_last_in[0] = 0;
+    
+
+    s_valid_in[1] = 1;
+    s_data_in[1] = 4'hB;
+    s_qos_in[1] = 2'b10; 
+    s_last_in[1] = 0;
+    
+    #10; 
+    
+
+
+    s_data_in[1] = 4'hD;
+    s_last_in[1] = 1;                           //End transaction
+    #10;
+    s_valid_in[1] = 0;
+    s_valid_in[0] = 1;
+    #10;
+    s_data_in[0] = 4'hC;
+    s_last_in[0] = 1;                           //End transaction
+
+    #10
+    s_valid_in = 0;
+    s_last_in = 0;
+    #20;
+    
+    $display("--------------Test 2: same_qos ----------------");
+    
+    // Both streams same qos 2=2
+    s_valid_in[0] = 1;
+    s_data_in[0] = 4'h9;
+    s_qos_in[0] = 2'b10;  
+    s_last_in[0] = 0;
+    
+    s_valid_in[1] = 1;
+    s_data_in[1] = 4'h8;
+    s_qos_in[1] = 2'b10; 
+    s_last_in[1] = 0;
+    
+    #10;
+    
+    s_data_in[0] = 4'h7;
+    s_last_in[0] = 1;
+    s_data_in[1] = 4'h6;
+    s_last_in[1] = 1;
+    
+    #10;
+    
+    s_valid_in = 0;
+    s_last_in = 0;
+    #30;
+    
+    $display("---------------- FINISH -----------------");
+    $finish;
+end
+
+// Monitor
+always @(posedge clk) begin    
+    for (int i =0; i < STREAM_COUNT; i++) begin
+      $display("Time=%0t: IN[%0d]: data=0x%h, qos=%0d, last=%0d, valid_in =%0d, ready_out=%0d \n\n", 
+                $time, i, s_data_in[i], s_qos_in[i], s_last_in[i], s_valid_in[i], s_ready_out[i]);
+      $display("Time=%0t: OUT: id=%0d, qos=%0d, data=0x%h, last=%0d, valid_out =%0d, ready_in = %0d \n\n", 
+                $time, m_id_out, m_qos_out, m_data_out, m_last_out, m_valid_out, m_ready_in);                   
+        
+    end
+end
+
+
+
+endmodule
---
+++ top.sv
@@ -0,0 +1,113 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+module stream_arbiter_w_qos #(
+ parameter   T_DATA_WIDTH = 4,
+      T_QOS__WIDTH = 2,
+      STREAM_COUNT = 2,
+      T_ID___WIDTH = $clog2(STREAM_COUNT)
+)(
+ input logic clk,
+ input logic rst_n,
+
+ input logic [T_DATA_WIDTH-1:0] s_data_in [STREAM_COUNT-1:0],
+ input logic [T_QOS__WIDTH-1:0] s_qos_in [STREAM_COUNT-1:0],            // not [T_QOS__WIDTH-1:0] because we need pointer when we complete transac and old data is not neseccery
+ input logic [STREAM_COUNT-1:0] s_last_in,                            // for this example s_qos_in = 3: the highest priority, s_qos_in = 1 the lowest priority, s_qos_in = 5: we dont use this data
+ input logic [STREAM_COUNT-1:0] s_valid_in,
+ output logic [STREAM_COUNT-1:0] s_ready_out,
+
+ output logic [T_DATA_WIDTH-1:0] m_data_out,
+ output logic [T_QOS__WIDTH-1:0] m_qos_out,
+ output logic [T_ID___WIDTH-1:0] m_id_out,
+ output logic m_last_out,
+ output logic m_valid_out,
+ input logic m_ready_in
+ 
+);
+typedef enum logic {
+    ST_IDLE = 1'b0,
+    ST_ACTIVE = 1'b1
+} state_t;
+
+state_t state;
+logic [T_ID___WIDTH-1:0] selected_stream;
+logic [T_QOS__WIDTH-1:0] selected_qos;
+logic [T_ID___WIDTH-1:0] rr_pointer;
+
+logic [T_ID___WIDTH-1:0] index;
+logic [T_ID___WIDTH-1:0] next_stream;
+logic stream_found;
+
+always_comb begin
+    logic [T_QOS__WIDTH-1:0] max_qos;
+    logic [STREAM_COUNT-1:0] candidate_mask;
+    
+
+    max_qos = 0;
+    for (int i = 0; i < STREAM_COUNT; i++) begin
+        if (s_valid_in[i] && s_qos_in[i] != 0) begin
+            if (s_qos_in[i] > max_qos) begin
+                max_qos = s_qos_in[i];
+            end
+        end
+    end
+    
+
+    candidate_mask = 0;
+    for (int i = 0; i < STREAM_COUNT; i++) begin
+        if (s_valid_in[i]) begin
+            if (max_qos != 0) begin
+
+                if (s_qos_in[i] == max_qos || s_qos_in[i] == 0) begin
+                    candidate_mask[i] = 1'b1;
+                end
+            end else begin
+
+                if (s_qos_in[i] == 0) begin
+                    candidate_mask[i] = 1'b1;
+                end
+            end
+        end
+    end
+    
+
+    stream_found = 1'b0;
+    next_stream = rr_pointer;
+    
+    for (int i = 1; i <= STREAM_COUNT; i++) begin
+  
+        index = (rr_pointer + i) % STREAM_COUNT;
+        
+        if (candidate_mask[index]) begin
+            next_stream = index;
+            stream_found = 1'b1;
+            break;
+        end
+    end
+end
+
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        state <= ST_IDLE;
+        rr_pointer <= 0;
+        selected_stream <= 0;
+        selected_qos <= 0;
+    end else begin
+        case (state)
+            ST_IDLE: begin
+                if (stream_found && m_ready_in) begin
+                    state <= ST_ACTIVE;
+                    selected_stream <= next_stream;
+                    selected_qos <= s_qos_in[next_stream];
+                    rr_pointer <= next_stream;
+                end
+            end
+            
+            ST_ACTIVE: begin
+                if (m_valid_out && m_ready_in && m_last_out) begin
+                    state <= ST_IDLE;
+                end
+            end
+        endcase
+    end
+end
